6018138cdb430a18c5191a4a57febb88dd4ff1be
Lesz@Xi <codewithafar@gmail.com>
2025-06-11 14:08:26 +0800

    Fix React 19 compatibility and deployment issues

---
+++ components/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-full h-screen bg-black flex items-center justify-center">
+      <div className="text-white">Loading 3D Scene...</div>
+    </div>
+  );
+}
